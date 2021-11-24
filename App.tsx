a2e5e7b9346939fd5ec1125a4cb55fb65a7c021a
mp-gumi <basil725woodstock@gmail.com>
2021-11-24 15:38:30 +0900

    feat: add tab icon

--- App.tsx
+++ App.tsx
@@ -3,15 +3,30 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import Game from "./src/containers/Pages/Game";
 import GameConfig from "./src/containers/Pages/GameConfig";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === "Game") {
+              iconName = "gamepad";
+            } else if (route.name === "Settings") {
+              iconName = "gear";
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Game" component={Game} />
-        <Tab.Screen name="設定" component={GameConfig} />
+        <Tab.Screen name="Settings" component={GameConfig} />
       </Tab.Navigator>
     </NavigationContainer>
   );
